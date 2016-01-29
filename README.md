3f8dff8f1aac30d51506fa12af33d3822254f432
Federico G. Schwindt <fgsch@lodoss.net>
2016-01-29 20:08:42 +0000

    Initial revision

---
+++ README.md
@@ -0,0 +1,83 @@
+libvmod-crypto
+==============
+
+[![Gitter](https://badges.gitter.im/Join Chat.svg)](https://gitter.im/fgsch/libvmod-crypto?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![Build Status](https://travis-ci.org/fgsch/libvmod-crypto.svg?branch=master)](https://travis-ci.org/fgsch/libvmod-crypto)
+
+## About
+
+A Varnish 4.x VMOD to compute message digests and keyed-hash message
+authentication codes (HMAC).
+
+## Requirements
+
+To build this VMOD you will need:
+
+* make
+* a C compiler, e.g. GCC or clang
+* pkg-config
+* python-docutils
+* libvarnishapi-dev in Debian/Ubuntu or varnish-libs-devel in CentOS/RedHat
+* libssl-dev in Debian/Ubuntu or openssl-devel in CentOS/RedHat
+
+If you are building from Git, you will also need:
+
+* autoconf
+* automake
+* libtool
+
+In addition, to run the tests you will need:
+
+* varnish
+
+If varnish is installed in a non-standard prefix you will also need
+to set `PKG_CONFIG_PATH` to the directory where **varnishapi.pc** is
+located before running `autogen.sh` and `configure`.  For example:
+
+```
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+```
+
+## Installation
+
+### From a tarball
+
+To install this VMOD, run the following commands:
+
+```
+./configure
+make
+make check
+sudo make install
+```
+
+The `make check` step is optional but it's good to know whether the
+tests are passing on your platform.
+
+### From the Git repository
+
+To install from Git, clone this repository by running:
+
+```
+git clone https://github.com/fgsch/libvmod-crypto
+```
+
+And then run `./autogen.sh` followed by the instructions above for
+installing from a tarball.
+
+## Example
+
+```
+import crypto;
+
+sub vcl_recv {
+	if (crypto.hmac_sha256("secret",
+	    req.http.host + req.url + req.http.timestamp) != req.http.hmac) {
+		return (synth(401));
+	}
+}
+```
+
+## License
+
+This VMOD is licensed under BSD license. See LICENSE for details.
---
+++ src/vmod_crypto.c
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2016, Federico G. Schwindt <fgsch@lodoss.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <openssl/crypto.h>
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
+#include <openssl/whrlpool.h>
+
+#include "cache/cache.h"
+
+#include "vrt.h"
+
+#include "vcc_if.h"
+
+#define SHA1_CBLOCK			SHA_CBLOCK
+#define SHA1_CTX			SHA_CTX
+#define SHA1_DIGEST_LENGTH		SHA_DIGEST_LENGTH
+#define SHA224_CBLOCK			SHA256_CBLOCK
+#define SHA224_CTX			SHA256_CTX
+#define SHA384_CBLOCK			SHA512_CBLOCK
+#define SHA384_CTX			SHA512_CTX
+#define WHIRLPOOL_CBLOCK		0
+
+#define CBLOCK_MAX			128
+#define DIGEST_LENGTH_MAX		64
+
+
+typedef int init_f(void *);
+typedef int update_f(void *, const void *, unsigned long);
+typedef int final_f(unsigned char *, void *);
+
+struct hash_tbl {
+	const char	*name;
+	int		 cblock;
+	int		 digest_length;
+	init_f		*init;
+	update_f	*update;
+	final_f		*final;
+};
+
+
+static char *
+hash_common(VRT_CTX, const struct hash_tbl *h, void *c, VCL_STRING s)
+{
+	unsigned char m[DIGEST_LENGTH_MAX];
+	char *p;
+	int i;
+
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+	if (!s) {
+		VSLb(ctx->vsl, SLT_Error,
+		    "crypto.hash_%s: No input", h->name);
+		return (NULL);
+	}
+	p = WS_Alloc(ctx->ws, h->digest_length * 2 + 1);
+	if (!p) {
+		VSLb(ctx->vsl, SLT_Error,
+		    "crypto.hash_%s: Out of workspace", h->name);
+		return (NULL);
+	}
+	h->init(c);
+	h->update(c, s, strlen(s));
+	h->final(m, c);
+	for (i = 0; i < h->digest_length; i++)
+		sprintf(&p[i * 2], "%02x", m[i]);
+	return (p);
+}
+
+static char *
+hmac_common(VRT_CTX, const struct hash_tbl *h, void *c, VCL_STRING key,
+    VCL_STRING message)
+{
+	unsigned char i_pad[CBLOCK_MAX], o_pad[CBLOCK_MAX];
+	unsigned char m[DIGEST_LENGTH_MAX];
+	int i, key_len;
+	char *p;
+
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+	if (!key || !message) {
+		VSLb(ctx->vsl, SLT_Error,
+		    "crypto.hmac_%s: Missing %s", h->name,
+		    !key ? "key" : "message");
+		return (NULL);
+	}
+	p = WS_Alloc(ctx->ws, h->digest_length * 2 + 3);
+	if (!p) {
+		VSLb(ctx->vsl, SLT_Error,
+		    "crypto.hmac_%s: Out of workspace", h->name);
+		return (NULL);
+	}
+	key_len = strlen(key);
+	/* Hash the key if it is longer than the block size. */
+	if (key_len > h->cblock) {
+		h->init(c);
+		h->update(c, key, key_len);
+		h->final(m, c);
+		key = (const char *)m;
+		key_len = h->digest_length;
+	}
+	memset(i_pad, 0, sizeof(i_pad));
+	memset(o_pad, 0, sizeof(o_pad));
+	memcpy(i_pad, key, key_len);
+	memcpy(o_pad, key, key_len);
+	/* XOR key with inner and outer padding constants. */
+	for (i = 0; i < h->cblock; i++) {
+		i_pad[i] ^= 0x36;
+		o_pad[i] ^= 0x5c;
+	}
+	/* Inner hash. */
+	h->init(c);
+	h->update(c, i_pad, h->cblock);
+	h->update(c, message, strlen(message));
+	h->final(m, c);
+	/* Outer hash. */
+	h->init(c);
+	h->update(c, o_pad, h->cblock);
+	h->update(c, m, h->digest_length);
+	h->final(m, c);
+	sprintf(p, "0x");
+	for (i = 0; i < h->digest_length; i++)
+		sprintf(&p[i * 2 + 2], "%02x", m[i]);
+	return (p);
+}
+
+#define VMOD_CRYPTO_HASH_ALL						\
+H(md4, MD4)								\
+H(md5, MD5)								\
+H(ripemd160, RIPEMD160)							\
+H(sha1, SHA1)								\
+H(sha224, SHA224)							\
+H(sha256, SHA256)							\
+H(sha384, SHA384)							\
+H(sha512, SHA512)							\
+H(whirlpool, WHIRLPOOL)
+
+#define H(n,p) 								\
+HASH_##p,
+enum {
+VMOD_CRYPTO_HASH_ALL
+};
+#undef H
+
+#define H(n,p) 								\
+{ #n, p##_CBLOCK, p##_DIGEST_LENGTH,					\
+  (init_f *)p##_Init, (update_f *)p##_Update, (final_f *)p##_Final },
+const struct hash_tbl hash_tbl[] = {
+VMOD_CRYPTO_HASH_ALL
+};
+#undef H
+
+#define H(n,p)								\
+VCL_STRING __match_proto__(td_crypto_hash_##n)				\
+vmod_hash_##n(VRT_CTX, VCL_STRING s)					\
+{									\
+	p##_CTX c;							\
+	return (hash_common(ctx, &hash_tbl[HASH_##p], &c, s));		\
+}
+VMOD_CRYPTO_HASH_ALL
+#undef H
+
+#define H(n,p)								\
+VCL_STRING __match_proto__(td_crypto_hmac_##n)				\
+vmod_hmac_##n(VRT_CTX, VCL_STRING key, VCL_STRING message)		\
+{									\
+	p##_CTX c;							\
+	return (hmac_common(ctx, &hash_tbl[HASH_##p], &c, key,		\
+	    message));							\
+}
+VMOD_CRYPTO_HASH_ALL
+#undef H
